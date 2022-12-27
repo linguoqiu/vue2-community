de9b3174e12042391d6c904b3def56068820995d
gavin <lin.guoqiu@iwhalecloud.com>
2022-12-27 15:17:48 +0800

    feat: 引入layui

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,52 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <div class="layui-container">
+      <form class="layui-form layui-form-pane" action="">
+        <div class="layui-form-item">
+          <label class="layui-form-label">用户名</label>
+          <div class="layui-input-block">
+            <input type="text" name="title" required  lay-verify="required" placeholder="请输入用户名" autocomplete="off" class="layui-input">
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">密码</label>
+          <div class="layui-input-block">
+            <input type="password" name="title" required  lay-verify="required" placeholder="请输入密码" autocomplete="off" class="layui-input">
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">验证码</label>
+          <div class="layui-input-block">
+            <input type="text" name="title" required  lay-verify="required" placeholder="请输入验证码" autocomplete="off" class="layui-input">
+          </div>
+        </div>
+        <button type="button" class="layui-btn">立即登录</button>
+        <a class="forget-link" href="">忘记密码</a>
+      </form>
+    </div>
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background-color: #f2f2f2;
+}
+.layui-container {
+  background: #fff;
 }
 
-nav {
-  padding: 30px;
+.layui-form-pane {
+  margin: 0 12px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+input {
+  width: 190px;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.forget-link {
+  margin-left: 10px;
+  &:hover {
+    color: #009688;
   }
 }
 </style>
