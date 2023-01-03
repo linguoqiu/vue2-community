8278ffc798481d7f56eeff74dfc825da75731a84
Gavin <876575488@qq.com>
2023-01-03 22:33:26 +0800

    feat: prettier fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,24 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always',
+      },
+    ],
+  },
+};
--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,52 @@
         <div class="layui-form-item">
           <label class="layui-form-label">用户名</label>
           <div class="layui-input-block">
-            <input type="text" name="title" required  lay-verify="required" placeholder="请输入用户名" autocomplete="off" class="layui-input">
+            <input
+              type="text"
+              name="title"
+              required
+              lay-verify="required"
+              placeholder="请输入用户名"
+              autocomplete="off"
+              class="layui-input"
+              v-model="name"
+            />
           </div>
         </div>
         <div class="layui-form-item">
           <label class="layui-form-label">密码</label>
           <div class="layui-input-block">
-            <input type="password" name="title" required  lay-verify="required" placeholder="请输入密码" autocomplete="off" class="layui-input">
+            <input
+              type="password"
+              name="title"
+              required
+              lay-verify="required"
+              placeholder="请输入密码"
+              autocomplete="off"
+              class="layui-input"
+              v-model="password"
+            />
           </div>
         </div>
         <div class="layui-form-item">
           <label class="layui-form-label">验证码</label>
           <div class="layui-input-inline">
-            <input type="text" name="title" required  lay-verify="required" placeholder="请输入验证码" autocomplete="off" class="layui-input">
+            <input
+              type="text"
+              name="title"
+              required
+              lay-verify="required"
+              placeholder="请输入验证码"
+              autocomplete="off"
+              class="layui-input"
+              v-model="code"
+            />
           </div>
-          <div class="layui-form-mid" v-html="svg"></div>
+          <div
+            class="layui-form-mid svg"
+            v-html="svg"
+            @click="getCaptcha"
+          ></div>
         </div>
         <button type="button" class="layui-btn">立即登录</button>
         <a class="forget-link" href="">忘记密码</a>
@@ -28,24 +59,45 @@
   </div>
 </template>
 <script>
-import axios from 'axios'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  data () {
+  data() {
     return {
-      svg: ''
-    }
+      svg: '',
+      name: '',
+      password: '',
+      code: '',
+      errorMsg: [],
+    };
   },
-  mounted () {
-    axios.get('http://localhost:3000/api/getCaptcha').then(res => {
-      console.log(res)
-      if (res?.status === 200 && res?.data?.code === 200) {
-        this.svg = res.data.data
+  mounted() {
+    this.getCaptcha();
+  },
+  methods: {
+    getCaptcha() {
+      axios.get('http://localhost:3000/api/getCaptcha').then((res) => {
+        console.log(res);
+        if (res?.status === 200 && res?.data?.code === 200) {
+          this.svg = res.data.data;
+        }
+      });
+    },
+    checkForm() {
+      this.errorMsg = [];
+      if (!this.name) {
+        this.errorMsg.push('登录密码为空！');
       }
-    })
-  }
-}
+      if (!this.password) {
+        this.errorMsg.push('密码为空！');
+      }
+      if (!this.code) {
+        this.errorMsg.push('验证码为空！');
+      }
+    },
+  },
+};
 </script>
 <style lang="scss" scoped>
 #app {
@@ -68,5 +120,11 @@ input {
   &:hover {
     color: #009688;
   }
+}
+
+.svg {
+  position: relative;
+  top: -15px;
+  cursor: pointer;
 }
 </style>
