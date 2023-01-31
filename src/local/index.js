35affa489103e4002aba1a0034cab67c9b38c916
Gavin <876575488@qq.com>
2023-01-31 22:18:59 +0800

    feat: update login page

--- src/local/index.js
+++ src/local/index.js
@@ -6,15 +6,17 @@ import { Validator } from 'vee-validate';
 const dictionary = {
   'zh-CN': {
     messages: {
-      required: (field) => `请输入${field}`,
+      required: (field) => '请输入' + field,
       email: () => '请输入正确的邮箱格式',
-      min: (field, value) => `${field}至少需要${value?.[0]}位`,
-      length: '验证码长度要求4位',
+      min: () => '不符合最小长度要求',
+      length: (field, args) => field + '长度要求' + args + '个字符',
+      confirmed: () => '两次输入内容不一致',
     },
     attributes: {
       email: '邮箱',
       password: '密码',
-      name: '账号',
+      name: '昵称',
+      username: '账号',
       code: '验证码',
     },
   },
--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import Vue from 'vue';
+import VeeValidate, { Validator } from 'vee-validate';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import './local';
+import axios from 'axios';
+
+Vue.use(VeeValidate);
+
+const validator = new Validator();
+validator.localize('zh-CN');
 
 Vue.config.productionTip = false;
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV !== 'production'
+    ? 'http://localhost:3000/api'
+    : 'http://domain:port/api';
 
 new Vue({
   router,
